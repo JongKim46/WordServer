d8700272b13cbf10bddeaf12e7df15ce75d7ed7a
Jong Kim <j-kim@fores.jp>
2023-08-02 10:59:03 +0900

    8/2 SQL LOG 追加

--- src/main/java/com/example/word_ServerApi/Word/Repository/WordRepository.java
+++ src/main/java/com/example/word_ServerApi/Word/Repository/WordRepository.java
@@ -2,12 +2,10 @@ package com.example.word_ServerApi.Word.Repository;
 
 import com.example.word_ServerApi.Word.Mapper.WordMapper;
 import com.example.word_ServerApi.Word.Model.WordResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -17,7 +15,6 @@ public class WordRepository {
     WordMapper wordmapper;
 
     public List<WordResponse> findAll(String wordLevel){
-        log.info("wordLevel : " + wordLevel);
         return wordmapper.findAll(wordLevel);
     }
 }
--- src/main/java/com/example/word_ServerApi/Word/Service/ApiWordService.java
+++ src/main/java/com/example/word_ServerApi/Word/Service/ApiWordService.java
@@ -2,13 +2,10 @@ package com.example.word_ServerApi.Word.Service;
 
 import com.example.word_ServerApi.Word.Model.WordResponse;
 import com.example.word_ServerApi.Word.Repository.WordRepository;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.beans.Transient;
 import java.util.List;
 
 @Service
